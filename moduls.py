d0e1d1cda4d3fa722d9b151eccab8b1438fda927
Ahmedessaa <159327594+Ahmedessaa@users.noreply.github.com>
2025-05-05 21:19:52 +0300

    Create moduls.py

---
+++ moduls.py
@@ -0,0 +1,17 @@
+import Mario
+import vowels
+import Multiply
+import Project
+import multiply2
+import Email_Valid
+import Check_User
+
+
+Vowls.count_vowels(Vowls.string)
+Multiply.multiplication_table(Multiply.num)
+
+Mario.mario_pyramid(Mario.height)
+Project.Project()
+
+Email_Valid.name_is_valid(Email_Valid.name)
+Check_User.Check_User()
